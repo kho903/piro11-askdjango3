9565ca96a3f4d09a5cdfb3630f6df9da9e66fed0
kho903 <gmldnr2222@naver.com>
2019-07-19 18:50:07 +0900

    8강 settings

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,8 @@ from .models import Post
 # Register your models here.
 
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at', 'updated_at']
 
-
-admin.site.register(Post)
+#admin.site.register(Post, PostAdmin)
