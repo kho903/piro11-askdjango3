7fb2a89c9f2621200028f2e2d81bd76a8f5ded21
kho903 <gmldnr2222@naver.com>
2019-07-19 19:56:29 +0900

    blog.views.post_list 뷰에서 포스티 목록 구현

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
     return render(request, 'blog/post_list.html')
 
+
+def post_list(request):
+    qs=Post.objects.all()
+    return render(request, 'blog/post_list.html',{
+        'post_list' : qs
+    })
