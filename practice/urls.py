6abb4957c140d9a14a13a4457e2731df0442d223
kho903 <gmldnr2222@naver.com>
2019-07-20 01:24:20 +0900

    shop 앱 생성/등록

--- practice/urls.py
+++ practice/urls.py
@@ -28,7 +28,8 @@ urlpatterns =[
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/',include('accounts.urls')),
     url(r'^blog/', include('blog.urls')),
-    url(r'^dojo/', include('dojo.urls'))
+    url(r'^dojo/', include('dojo.urls')),
+    url(r'^shop/', include('shop.urls')),
 ]
 
 if settings.DEBUG:
