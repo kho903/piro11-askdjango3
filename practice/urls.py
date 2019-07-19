b20a2df9e3e31f29e19f27ab717fb3c97e5cec3b
kho903 <gmldnr2222@naver.com>
2019-07-19 20:44:10 +0900

    blog.bodels.Post 모델에 기본정렬 지정

--- practice/urls.py
+++ practice/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 # """
-# from django.contrib import admin
-# from django.urls import path,include
+#from django.urls import path,include
+
 #
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
 #     path('', include('core.urls'), name='core'),
 # ]
-
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -29,3 +29,9 @@ urlpatterns =[
     url(r'^blog/', include('blog.urls')),
     url(r'^dojo/', include('dojo.urls'))
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/',include(debug_toolbar.urls)),
+    ]
