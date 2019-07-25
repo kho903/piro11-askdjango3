86d22485e0e974893ac178cbf113cc6d2edc65b7
kho903 <gmldnr2222@naver.com>
2019-07-25 15:41:59 +0900

    blog.vies.post_new/post_edit 뷰를 구현

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import re_path
 
-from blog import views_cbv
+from . import views_cbv
 from . import views
 
 urlpatterns=[
-    path('', views.post_list, name='post_list'),
-    path('<int:id>', views.post_detail, name='post_detail'),
-    path(r'^cbv/new/$', views_cbv.post_new),
+    re_path(r'^$', views.post_list, name='post_list'),
+    re_path(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
+
+    re_path(r'^new/$', views.post_new, name='post_new'),
+    re_path(r'^(?P<id>\d+)/edit/$', views.post_edit, name='post_edit'),
+
+    re_path(r'^cbv/new/$', views_cbv.post_new),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.http import Http404, HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -12,7 +13,7 @@ def post_list(request):
     if q:
         qs = qs.filter(title__icontains=q)
 
-    response = render(request, 'blog/post_list.html',{
+    response = render(request, 'blog/post_list.html', {
         'post_list': qs,
         'q': q,
     })
@@ -30,4 +31,34 @@ def post_detail(request, id):
 
     return render(request, 'blog/post_detail.html',{
         'post':post,
+    })
+
+
+def post_new(request):
+    if request.method =='POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            #post.user = request.user
+            return redirect(post)  #post.get_absolute_url() => post detail
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {
+        'form': form,
+    })
+
+
+def post_edit(request, id):
+    post = get_object_or_404(Post,id=id)
+
+    if request.method =='POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            #post.user = request.user
+            return redirect(post)  #post.get_absolute_url() => post detail
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {
+        'form': form,
     })
