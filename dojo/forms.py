68ea5c3e7fc28ed4e9e15a4f892e924ed421744a
kho903 <gmldnr2222@naver.com>
2019-07-25 16:31:23 +0900

    dojo.models.Post 모델에 user_agent 필드 추가

--- dojo/forms.py
+++ dojo/forms.py
@@ -7,8 +7,10 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content']
-
+        fields = ['title', 'content', 'user_agent']
+        widgets={
+            'user_agent': forms.HiddenInput,
+        }
         '''
         def save(self, commit=True):
             self.instance = Post(**self.cleaned_data)
