24118e2128fd6d4cbcd74730a91de939631951dc
kho903 <gmldnr2222@naver.com>
2019-07-25 11:00:26 +0900

    dojo.forms.PostForm 클래스 정의

--- dojo/models.py
+++ dojo/models.py
@@ -5,6 +5,6 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
