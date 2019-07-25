4faa23472492738cd4f6e2a2f4ab22fbf9b7e9eb
kho903 <gmldnr2222@naver.com>
2019-07-25 16:20:10 +0900

    dojo.models.GameUser 모델을 통해 unique_together 예시 추가

--- dojo/models.py
+++ dojo/models.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 
@@ -16,3 +17,16 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
+class GameUser(models.Model):
+    server_name = models.CharField(max_length=10,
+                                   choices=(
+                                        ('A', 'A서버'),
+                                        ('B', 'B서버'),
+                                        ('C', 'C서버'),
+                                    ))
+    username = models.CharField(max_length=20, validators=[MinLengthValidator(3)])
+
+    class Meta:
+        unique_together = [
+            ('server_name', 'username'),
+        ]
