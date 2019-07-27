574aba35509177d3d0272590158ff5aedcf245b9
kho903 <gmldnr2222@naver.com>
2019-07-27 17:35:53 +0900

    filesystem static finder 설정 및 static 샘플 추가

--- practice/settings.py
+++ practice/settings.py
@@ -130,7 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'practice', 'static'),
+]
 
 INTERNAL_IPS = ['127.0.0.1']
 
