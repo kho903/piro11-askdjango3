0764ed68a4e3728bf609877f06ceee3760cee8c4
kho903 <gmldnr2222@naver.com>
2019-08-01 11:10:05 +0900

    media파일 서빙 지원

--- practice/urls.py
+++ practice/urls.py
@@ -23,6 +23,7 @@ Including another URLconf
 
 from django.conf import settings
 #from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.urls import path, include, re_path
 from django.contrib import admin
 from django.shortcuts import redirect
@@ -41,6 +42,9 @@ urlpatterns=[
     re_path(r'^dojo/', include('dojo.urls'), name='dojo'),
     re_path(r'^shop/', include('shop.urls'), name='shop'),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
