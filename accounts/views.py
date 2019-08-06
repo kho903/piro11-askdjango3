9f694c3590194564f6e57659f6c33b26935c75e3
kho903 <gmldnr2222@naver.com>
2019-08-06 15:28:05 +0900

    프로필 조금 수정

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -21,5 +22,6 @@ def signup(request):
     })
 
 
+@login_required
 def profile(request):
     return render(request, 'accounts/profile.html')
